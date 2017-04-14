bfa4139fbee5a7097aa77b46cdf7dfe25ef02052
Christopher Gomez <c.gomez364@yahoo.com>
2017-04-14 11:27:19 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,25 +9,3 @@ $(document).ready(function(){
 	});
 	
 });
-
-function init() {
-	
-	// start up after 2sec no matter what
-    window.setTimeout(function(){
-        start();
-    }, 2000);
-}
-
-// fade in experience
-function start() {
-	
-	$('body').removeClass("loading").addClass('loaded');
-	
-}
-
-$(window).load(function() {
-	
-	// fade in page
-	init();
-	
-});
